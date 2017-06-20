5093ce5405cac88652c508f2acbb7a6e68cfdd5f
Luis <luis_matawd@hotmail.com>
2017-06-20 00:44:19 -0500

    m

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Creando nuevos usuarios
+User.create name: 'Jose', status: :active, kind: :salesman, email: 'salesman@tester.com', password: 123456
+User.create name: 'Manuel', status: :active, kind: :salesman, email: 'salesman2@tester.com', password: 123456
+
+User.create name: 'Marcos', status: :active, kind: :manager, email: 'manager@tester.com', password: 123456
+
+#Creando productos de ejemplo
+Product.create name: 'Smarphone', description: 'Un nuevo smarphone', status: :active
+Product.create name: 'Tablet', description: 'Un nuevo Tablet', status: :active
+
+Discount.create name: 'Descuento carnaval', description: 'Aplica descuento', value: '10',  status: :active
